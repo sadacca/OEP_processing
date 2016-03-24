70a3b886e5e1bbe48a2194be5234f53cb8dc2970
sadacca <bfsadacca@gmail.com>
2016-03-24 12:40:10 -0400

    made working extraction validated on MedAssociates based TTL triggers for use in directory with data (add UI get?)

--- data_extract_to_matlab/OEPevent2mat.m
+++ data_extract_to_matlab/OEPevent2mat.m
@@ -1,83 +1,26 @@
-function OEPcont_to_dat()
-% transmute an OEP .continuous file to a raw .dat file
+function OEPevent2mat()
+% transmute an OEP .event file into a matlab file
 % no input parameters
-% subtracts AUX channels (3, hardcoded)
+% extracts event#3 hardcoded
 %
-% written by Brian Sadacca 12-9-2015
+% written by Brian Sadacca 3-21-2016
 % last modified by ...
 
-%% get the data
-filename = dir('*CH*.continuous');
+%% get the data, assuming you're in the right directory
+filename = dir('all_channels.events');
+filedirectory = cd;
 
 %% load the data
 %get there
 
+[eventlabel, timestamps, info_events] = load_open_ephys_data_faster(filename.name);
+OEPon=timestamps((info_events.eventId == 0) & (info_events.eventType == 3));
+OEPoff=timestamps((info_events.eventId == 1) & (info_events.eventType == 3));
+    
+    
 
-if length(filename)>1  % if you've got multiple channels
-    for ii = 1:length(filename)
-        for jj = 1:length(filename)
-            if regexp(filename(ii).name,['CH',num2str(jj),'.'])
-                fileindex(ii)=jj;
-            end
-        end
-    end
-    
-    [xx fileorder]=sort(fileindex);
-    
-    %open the first channel
-    
-    [first_channel, timestamps, info_continuous] = load_open_ephys_data_faster(filename(fileorder(1)).name);
-    
-    
-    %initialize a big matrix for the data
-    incoming_data = zeros(length(filename),length(first_channel),'int16');
-    
-    %write the first channel to the full data matrix
-    incoming_data(1,:)= int16(first_channel);
-    
-    
-    
-    for ii = 2:length(filename) %repeat for the remainder of channels
-        
-        
-        
-        [next_channel timestamps, info_continuous] = load_open_ephys_data_faster(filename(fileorder(ii)).name);
-        
-        incoming_data(ii,:)=int16(next_channel);
-        
-    end
-    
-    %initialize a new .dat file
-    fid=fopen('sampledata.dat','w+');
-    
-    % write data to raw .dat file
-    fwrite(fid,incoming_data(:,:),'int16');
-    
-    % close that file
-    fclose(fid);
-    
-    %say that the job is done
-    disp(['files ',filename(1).name,' to ',filename(fileorder(end)).name,' converted'])
-    
-    
-else % else if you only have one channel or file
-    
-    % open the one file
-    [incoming_data] = load_open_ephys_data_faster(filename(1).name);
-    
-    % create/open a raw file
-    fid=fopen('sampledata.dat','w+');
-    
-    
-    % write data to raw file
-    fwrite(fid,incoming_data(1:end-3,:),'int16');
-    
-    % close that file
-    fclose(fid);
-    
-    %say that the job is done
-    disp(['file ',filename.name,' converted'])
-end
+save([filedirectory,'_extracted_event_times.mat'],'OEPon','OEPoff')
+
 exit
 end
 
