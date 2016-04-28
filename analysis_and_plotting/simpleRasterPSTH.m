7a3e2f6e80b42c57d74678333e96b7273f6791d9
sadacca <bfsadacca@gmail.com>
2016-04-28 18:02:39 -0400

    enabled psth plotting; need to finalize raster plotting

--- analysis_and_plotting/simpleRasterPSTH.m
+++ analysis_and_plotting/simpleRasterPSTH.m
@@ -54,6 +54,11 @@ clr=[.1 .9 .5;... %events 1 (black)
     .1 .6 .8;...  %events 3 (blue)
     .7 .6 .2];    %events 4 (red)
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% create legend for 'trial/event' types
+
+legendLabel = ['good + ';'neutral';'bad -  '];
+
 %% align the spikes to events of interest, for inclusion into a 4d matrix
 % psth4d -- a neuron x trialtype x trial# x time matrix
 
@@ -68,8 +73,8 @@ numBins = length(steps);
 
 for ii = numNeurons:-1:1
     for jj = numTrialTypes;
-        
-        eventsOfInterestIndex = ismember(medeventstamps,eventLabel{kk});
+                eventsOfInterestIndex = ismember(spike(ii).medlabels,eventLabel{jj});
+
         numTrials(ii,jj) = sum(eventsOfInterestIndex);
         
     end
@@ -81,36 +86,55 @@ psth4d = NaN(numNeurons,numTrialTypes,max(numTrials(:)),numBins);
 % now fill the intialized matrix with spike counts per bin
 
 
-for ii = size(spike,2):-1:1
-    for jj = length(eventLabel);
-        
-        for kk = numTrials:-1:1
+for ii = numNeurons:-1:1
+    for jj = numTrialTypes:-1:1;
+               
+        eventsOfInterestIndex = ismember(spike(ii).medlabels,eventLabel{jj});
+
+        for kk = numTrials(ii,jj):-1:1
             
             currentTrialEvent = eventtimesOfInterst(kk);
-            
-            eventPSTH(ii,kk,:) = histc(double(spike(ii).times)/sampleRate-currentTrialEvent,psthStart:bin:psthStop);
+        
+            psth4d(ii,jj,kk,:) = histc(double(spike(ii).times)/sampleRate-currentTrialEvent,psthStart:bin:psthStop);
             
         end
     end
 end
 
-%% initialize variables
-
-%% bin spikes into PSTH
+%% plot the mean psth (or other measure of spiking across trials)
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% create meanPSTH to plot data
 
-avgpsth = zeros(size(site,1),size(site,2),size(psth_spc2,4));
+meanPSTH = squeeze(nanmean(psth4d,3));
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% plot the line as a lower panel
+
+
+for nrn = neurons%1:size(site,1)
+    figure((nrn*100));
+    subplot(2,2,3:4)
+    for st = numTrialTypes:-1:1
+        
+        plot(-20:bin:60,squeeze(meanPSTH(nrn,st,:,:))/bin,'color',clr(st,:),'linewidth',2)
+        hold on
+        
+    end
+    xlim([plotStart plotEnd]), legend(legendLabel)
+    xlabel('seconds from cue')
+    ylabel('spikes/second')
+    
+    %plot([1,1],[0,30],'k--'),plot([0,0],[0,30],'k--'),plot([10,10],[0,30],'k--')
+    
+    
+end
 
 %% plot spikes on a trial-by-trial basis
 
 
-starttime = 19;
-endtime = 25;
-
 steps = [starttime:.001:endtime];
-maxtrials = 10;
 spikeduration = 0;
 %for cue onset
 
@@ -154,77 +178,3 @@ for nrn = neurons
 end
 
 
-
-starttime = 25;
-endtime = 31;
-
-steps = [starttime,endtime];
-maxtrials = 10;
-
-%for cue offset
-
-for nrn = neurons
-    cuect=0;
-    figure((nrn*100));
-    
-    
-    for cue = cues
-        for tr=1:length(site(nrn,cue).trials)
-            
-            %%plot spiketrains
-            
-            realspiketr = (site(nrn,cue).trials(tr).times(site(nrn,cue).trials(tr).times>starttime & site(nrn,cue).trials(tr).times<endtime))-20;
-            cuect=cuect+1;
-            
-            xx = realspiketr;
-            yy = ones(size(xx));
-            xx= xx'; yy = yy';
-            xpoints = [xx;xx+spikeduration;NaN(size(xx))];
-            ypoints = [yy+cuect-.4;yy+cuect+.4;NaN(size(yy))];
-            xpoints = xpoints(:);
-            ypoints = ypoints(:);
-            
-            subplot(2,2,2)
-            hold on,
-            
-            plot(xpoints,ypoints,'color',clr(cue,:))
-            
-            xlim([steps(1),steps(end)]-20)
-            
-        end
-    end
-    
-    plot([10,10],[1.1,tr+.9],'k--'),plot([0,0],[1.1,cuect+1],'k--')
-    xlabel('seconds from cue'),
-    ylabel('trial #')
-end
-
-
-
-
-for nrn = neurons%1:size(site,1)
-    figure((nrn*100));
-    subplot(2,2,3:4)
-    for st = cues
-        
-        clr=[.1 .9 .5;... %siren
-            .8 .1 .1;...  %tone
-            .1 .6 .8;...  %clicker
-            .7 .6 .2];    %whitenoise
-        
-        plot(-20:bin:60,sgolayfilt(squeeze(mean(squeeze(psth_spc2(nrn,st,:,:)),1))',2,51)/bin,'color',clr(st,:),'linewidth',2)
-        %plot(-20:bin:60,smooth(squeeze(mean(squeeze(psth_spc2(nrn,st,:,:)),1))',1/bin,'lowess')/bin,'color',clr(st,:),'linewidth',2)
-        hold on
-        
-        
-        %COMPILE AVERAGEPSTHS
-        
-    end
-    xlim([-15 40]), legend(['b+';'d-';'a ';'c '])
-    xlabel('seconds from cue')
-    ylabel('spikes/second')
-    
-    plot([1,1],[0,30],'k--'),plot([0,0],[0,30],'k--'),plot([10,10],[0,30],'k--')
-    
-    
-end
