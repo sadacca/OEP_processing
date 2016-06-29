85c50ec9dccc6b9adeae254b9a95dfaf350aded8
sadacca <bfsadacca@gmail.com>
2016-06-29 02:16:40 -0400

    spike extraction for new klusta programs (phy compatable)

---
+++ data_extract_to_matlab/kwik2mat_klusta.m
@@ -0,0 +1,74 @@
+%%  find the file
+[filename,filedirectory]=uigetfile('*.kwik');
+
+%goto the file directory
+cd(filedirectory)
+
+% for the first shank
+
+clear spike
+for pr = 0:7;
+probe = num2str(pr);
+
+%% find all the clusters and cluster indicies
+clusternumbers = double(h5read(filename, ['/channel_groups/',probe,'/spikes/clusters/main']));
+allclusters = unique(clusternumbers);
+
+%% of these, find MUA (group 1) and GOOD (group 2) clusters
+
+clustergroup = zeros(1,max(allclusters));
+
+for ac = allclusters'
+    thiscluster = num2str(ac);
+    clustergroup(ac+1) = h5readatt([filename(1:end-3),'wik'], ['/channel_groups/',probe,'/clusters/main/',thiscluster],'cluster_group');
+end
+
+ismua = find(clustergroup == 1)-1;
+isgood = find(clustergroup == 2)-1;
+
+ isgood  = [isgood,ismua]; %uncomment if you want to treat mua as good
+
+% extract all spiketimes from *.kwik file
+
+allspiketimes=h5read([filename(1:end-3),'wik'], ['/channel_groups/',probe,'/spikes/time_samples']);
+
+%get ready to put neurons into the multineuron data structure
+
+if exist('spike','var')
+    clusterindex = (1:length(isgood))+length(spike);
+else
+    clusterindex = 1:length(isgood);
+end
+
+%%get ready to extract select waveforms
+datFilename = [filename(1:end-4),'dat'];
+chansToExtract = [1 2 3 4]+(pr*4);
+window = [-25 50];
+nToRead = 100;
+chansInDat = 32;
+
+%% butter to filter data
+fs = 25000;
+Wn = (200/(fs/2));
+[B,A] = butter(5,Wn,'high');
+
+%slice out good waveforms into a spike.wave data structure
+for cl = 1:length(isgood)
+       spike(clusterindex(cl)).times = allspiketimes(clusternumbers==isgood(cl));
+       
+[meanWF, allWF] = readWaveformsFromDat(...
+    datFilename, chansInDat, spike(clusterindex(cl)).times,...
+    window, nToRead);
+filtmeanWF = filtfilt(B,A,meanWF(chansToExtract,:)');
+allWF = allWF(chansToExtract,:,:);
+        spike(clusterindex(cl)).waves = allWF;
+        spike(clusterindex(cl)).filtmwave = filtmeanWF;
+
+end
+end
+
+%% save  the spike file
+
+%save([filedirectory(end-25:end-1),'_extracted_tms_and_wvs.mat'],'spike')
+
+
